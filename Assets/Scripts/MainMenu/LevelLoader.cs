f98a2ce25c2da8939b9ca56b4e9c8fcb8975d4ce
Colton Henderson <coltonhenderson12@hotmail.com>
2021-11-03 22:33:06 -0500

    Edited Main Menu and Main Level

---
+++ Assets/Scripts/MainMenu/LevelLoader.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+
+    public void LoadMainMenuScene()
+    {
+        int sceneIndex = 0;
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    public void LoadLevel()
+    {
+        int sceneIndex = 1;
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    public void LoadWinScene()
+    {
+        int sceneIndex = 2;
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    public void LoadLoseScene()
+    {
+        int sceneIndex = 3;
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100 + "%";
+
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/StateMachine/PlayerTurnCardGameState.cs
+++ Assets/Scripts/StateMachine/PlayerTurnCardGameState.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerTurnCardGameState : CardGameState
 {
-    [SerializeField] Text _playerTurnTextUI = null;
+    [SerializeField] TextMeshProUGUI _playerTurnTextUI = null;
 
     int _playerTurnCount = 0;
 
